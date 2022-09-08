b3bac7111dc84c24d68625244d2772faee43e74a
tarhywoe <taiwosikiru91@gmail.com>
2022-09-08 16:01:49 -0700

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	/*string initialisation*/
-	char mystr[100] = "\"Programming is like building a multilingual puzzle \n";
+	char mystr[100] = "\"Programming is like building a multilingual puzzle";
 
 	/*writing the string to stout*/
 	puts(mystr);
