f044e90e6a2da2e73ab1819943edf5fea2a46efc
tarhywoe <taiwosikiru91@gmail.com>
2022-09-08 17:05:46 -0700

    6-size.c update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("char size: %lu bytes\n", sizeof(char));
-	printf("int size: %lu bytes\n", sizeof(int));
-	printf("long size: %lu bytes\n", sizeof(long));
-	printf("long long size: %lu bytes\n", sizeof(long long));
-	printf("float size: %lu bytes\n", sizeof(float));
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of a long int: %lu bytes\n", sizeof(long));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long));
+	printf("Size of a float: %lu bytes\n", sizeof(float));
 
 /*The sizes of the variable types are printed from the memory space*/
 	return (0);
