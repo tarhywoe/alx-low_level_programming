02acfe60cf4161102b3f1a7190e904e71f223207
tarhywoe <taiwosikiru91@gmail.com>
2022-09-13 21:49:20 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,14 @@ int print_last_digit(int n)
 	int l;
 
 	l = n % 10;
-	if (l < 10)
+	if (l < 0)
 	{
-		_putchar(-l + '0');
+		_putchar('0' + -l);
 		return (-l);
 	}
 	else
 	{
-		_putchar(l + '0');
+		_putchar('0' + l);
 		return (l);
 	}
 }
