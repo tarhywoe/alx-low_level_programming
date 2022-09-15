8c3c4f8426959d63d09da424cc3622638ac859c7
tarhywoe <taiwosikiru91@gmail.com>
2022-09-14 20:13:41 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	long int n1 = 0, n2 = 1, fn, Tn;
 
-	while (fn <= 4000000)
+	while (1)
 	{
 		fn = n1 + n2;
+		if (fn > 4000000)
+			break;
+
 		n1 = n2;
 		n2 = fn;
-
-
 		if ((fn % 2) == 0)
 			Tn += fn;
 
