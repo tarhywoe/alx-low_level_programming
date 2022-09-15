1db3f98fa5a4ba443570fe0125765bda2ec4acea
tarhywoe <taiwosikiru91@gmail.com>
2022-09-14 19:46:00 -0700

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print first 50 fibonacci numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int count;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to add even-valued fibonacci numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long int n1 = 1, n2 = 2, fn, Tn;
+
+	while (fn <= 4000000)
+	{
+		fn = n1 + n2;
+
+		if ((fn % 2) == 0)
+			Tn += fn;
+
+
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("%ld\n", Tn);
+
+	return (0);
+}
