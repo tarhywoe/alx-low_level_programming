a220fc609480f1ce38cef8d3735e15875da42134
tarhywoe <taiwosikiru91@gmail.com>
2022-09-20 14:09:38 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - this functions iterates and counts the strings in the array
+ *
+ * @s: thats the string arrays
+ * Return: returns the length of the array
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+
+	{
+	}
+
+	return (i);
+}
