66670391ff76a138549994209f1cadba2445c6d0
tarhywoe <taiwosikiru91@gmail.com>
2022-09-20 15:42:31 -0700

    4-print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string backwards
+ * 
+ * @s: the string to print
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
