408038420b004829df695cc6268b9f899f711bba
tarhywoe <taiwosikiru91@gmail.com>
2022-09-20 16:40:29 -0700

    7-putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of strings
+ *
+ * @str: the string to be printed
+ *
+ * Return: Empty
+ */
+void puts_half(char *str)
+{
+	int i, j, max;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	max = i;
+	j = max / 2;
+	while (j <= max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
