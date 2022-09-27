7249f9d40041bf7fed59fe36fdab30c10447b082
tarhywoe <taiwosikiru91@gmail.com>
2022-09-27 08:58:44 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int o = 0;
-	
-	while (s1[o] == s2[o])
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[o] == '\0' && s2 == '\0')
-			break;
-		o++;
+		s1++;
+		s2++;
 	}
-	
-	return (s1[o] - s2[o]);
+
+	return (*s1 - *s2);
 }
