57bd0b38303ef96a068752db3f68e653648039ee
tarhywoe <taiwosikiru91@gmail.com>
2022-09-27 08:13:30 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len 0;
+	int i, co;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	for (co = 0; co < n; co++)
+	{
+		dest[i + co] = src[co];
+		if (src[co] == '\0')
+			co = n;
+	}
 
 	return (dest);
 }
